0b0aa4a9908ba3e181c478b455b938cde43d5f28
Nashet <not@mmm.com>
2023-08-07 21:15:22 +0200

    Add inspector for entity array

---
+++ Editor/Inspectors/Ecs/EcsPackedEntityArrayInspector.cs
@@ -0,0 +1,49 @@
+﻿// ----------------------------------------------------------------------------
+// The MIT License
+// UnityEditor integration https://github.com/Leopotam/ecslite-unityeditor
+// for LeoECS Lite https://github.com/Leopotam/ecslite
+// Copyright (c) 2021-2022 Leopotam <leopotam@gmail.com>
+// ----------------------------------------------------------------------------
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Leopotam.EcsLite.UnityEditor.Inspectors
+{
+	sealed class EcsPackedEntityArrayInspector : EcsComponentInspectorTyped<EcsPackedEntity[]>
+	{
+		public override bool IsNullAllowed()
+		{
+			return true;
+		}
+
+		public override bool OnGuiTyped(string label, ref EcsPackedEntity[] value, EcsEntityDebugView entityView)
+		{
+			GUILayout.Label($"{label} has {value.Length} entitys:");
+			var entity = new EcsPackedEntityInspector();
+			for (int i = 0; i < value.Length; i++)
+			{
+				if (value[i].Unpack(entityView.World, out var unpackedEntity))
+				{
+					if (GUILayout.Button("Ping entity"))
+					{
+						EditorGUIUtility.PingObject(entityView.DebugSystem.GetEntityView(unpackedEntity));
+					}
+				}
+				else
+				{
+					if (value[i].EqualsTo(default))
+					{
+						EditorGUILayout.SelectableLabel("<Empty entity>", GUILayout.MaxHeight(EditorGUIUtility.singleLineHeight));
+					}
+					else
+					{
+						EditorGUILayout.SelectableLabel("<Invalid entity>", GUILayout.MaxHeight(EditorGUIUtility.singleLineHeight));
+					}
+				}
+			}
+			EditorGUILayout.Space();
+			return true;
+		}
+	}
+}
